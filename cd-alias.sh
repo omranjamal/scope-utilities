4b587020d5a3b9b2dd881ae8bcbd0c8c1e570ad8
Omran Jamal <o.jamal97@gmail.com>
2023-08-23 01:25:35 +0600

    removed remnant artworks

--- cd-alias.sh
+++
@@ -1,4 +0,0 @@
-function monocd {
-    DIR_PATH=$( [[ ! -z "$1" ]] && (mtab $1 --notab 3>&1 1>&2 2>&3) || (mtab --notab 3>&1 1>&2 2>&3) );
-    [[ ! -z "$DIR_PATH" ]] && cd $DIR_PATH || echo "CANCELLED";
-};
